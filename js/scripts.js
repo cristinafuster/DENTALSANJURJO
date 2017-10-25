22b6bbada7e0568c07af4df6ae174d83f3f637a4
Cristina Fuster <cristinafuster@gmail.com>
2017-10-25 10:43:21 +0200

    .devider

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
+$(window).on('load', inicializar);
 
+$(document).ready(function(){
+
+	$('.fa-bars').on('click', visibilidadMenu);
+
+	$('.desplazar').on('click', navegar);
+
+});
+
+
+function visibilidadMenu(){
+
+	$('#menu').toggleClass('abierto');
+}
+
+function inicializar(){
+
+	$('.overlay').fadeOut(2500);
+}
+
+function navegar(){
+
+	let objetivo = $(this).data('seccion');
+
+	let profundidad = $(objetivo).offset().top;
+
+	$('html').animate({
+		scrollTop:profundidad
+	}, 900);
+
+	$('.menu').removeClass('abierto');
+}
