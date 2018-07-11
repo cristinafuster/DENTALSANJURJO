3e2ca03894e9cd432abac58bd06dabbf4530ff86
Cristina Fuster <cristinafuster@gmail.com>
2018-07-11 14:24:50 +0200

    añadi video

--- js/scripts.js
+++ js/scripts.js
@@ -49,7 +49,7 @@ var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
   this.el = el;
   this.loopNum = 0;
-  this.period = parseInt(period, 10) || 2000;
+  this.period = parseInt(period, 10) || 800;
   this.txt = '';
   this.tick();
   this.isDeleting = false;
